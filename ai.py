e5360182db5b50d255e17edca3e0e06ec25c2a79
hitoyaCute <147114029+hitoyaCute@users.noreply.github.com>
2024-06-30 12:29:08 +0800

    Update ai.py
    new code and new error 😭😭😭😭

--- ai.py
+++ ai.py
@@ -1,348 +1,234 @@
-import numpy as np
-from math import tanh
-import time
-
 import random
-
-
-deepcopy = (
-    lambda x: [deepcopy(x[_]) for _, i in enumerate(x)]
-    if isinstance(x, (list, tuple))
-    else x
-)
-
-
-# activation functions
-inp = lambda x: x
-
-relu = lambda x: max(0, x)
-
-
-def node(ingoing_data: list[["weights","value"], ...],bias: float,activator: callable ) -> float:
-    return activator(sum([weight * value for weight, value in ingoing_data]) + bias)
-
-def agent_sort_by_score(arr):
-    if len(arr) <= 1:
-        return arr
-    else:
-        pivot = arr[len(arr) // 2][1]  # Choosing the middle element as the pivot
-        left = [x for x in arr if x[1] > pivot]
-        middle = [x for x in arr if x[1] == pivot]
-        right = [x for x in arr if x[1] < pivot]
-        return agent_sort_by_score(left) + middle + agent_sort_by_score(right)
-        
-
-
-
+import numpy as np
+from math import tanh,exp
+from copy import deepcopy
 from collections import defaultdict, deque
 
-class network:
-    """DAGs based network"""
-
-    def __init__(self, ins: int, outs: int) -> None:
-        """will make new network with input and output nodes only with no conection and biases set to 0"""
-        self.nodes = [[0, inp, [[None, 0]]] for _ in range(ins)] + [
-            [0, tanh, [[None, 0]]] for _ in range(outs)
-        ]
-
-        
-
-    def ev(self, inputs: list[int, ...]) -> list[int, ...]:
-        """evaluate and returns the final value of output neuron"""
-
-        out = []
-
-        # dict to store the list of value from parent neuron on name of on decendant neuron
-        value = {}
-
-        # puting the emvironment mesurements to input neuron
-        #for i,val in enumerate(inputs):
-#            value[i] = [0,val]
-
-        for nodeID in self.topoSort():
-        	
-            bias, func, connections = self.nodes[nodeID]
-
-            connector = [[0,inputs[nodeID]]] if func == inp else value[nodeID] if nodeID in value else [[0,0]]
-            
-            if connections and func != tanh:
-            	continue
-            	
-            
-            output = round(node(connector, bias, func),3)
-            
-            if func == tanh:
-                out.append(output)
-                continue  # to avoid runing the part because we know there is no more neuron
-            
-
-            # this part will save the output to the list of output on name of decendance so it can use it directly
-            for dest, wieght in deepcopy(connections):
-                if dest:  # if destination node is not None
-                    if dest in value:
-                        value[dest] += [output, wieght]
-                    else:
-                        value[dest] = [[output, wieght]]
-        return out
-
-    def add_hiden_node(
-        self,
-        bias: float,
-        srcs: list["parent_node: int, weight: float"],
-        dest: list["decendant_node: int, weight: float"],
-    ) -> None:
-        # adds new connection from parent node to new node
-        self.nodes[srcs[0]][2].append([len(self.nodes), srcs[1]])
-
-        # make new node with connection to decendant node
-        self.nodes.append((bias, relu, [dest]))
-
-    def add_connection(self, nodeID: int, dest: int, weight: float = 1.0) -> None:
-
-        existing_connections = deepcopy(self.nodes[nodeID][2])
-        temp = []
-        notfound = True
-        for node, i in existing_connections:
-            if node == dest:
-                notfound = False
-                out = weight
-            else:
-                out = i
-            temp.append([node, out])
-        if notfound:
-            temp.append([dest, weight])
-
-        return temp
-
-    def change_bias(self, nodeId: int, bias: float):
-        self.nodes[nodeId][0] = bias
-
-    def topoSort(self):
-        # Step 1: Initialize the graph and in-degree dictionary
-        graph = defaultdict(list)
-        in_degree = defaultdict(int)
-
-        # Step 2: Populate the graph and in-degree based on input_data
-        for index, (*a, descendants) in enumerate(self.nodes):
-            if descendants:
-                for descendant in descendants:
-                    if not descendant:
-                        continue
-                    graph[index].append(descendant[0])
-                    in_degree[descendant[0]] += 1
-
-            # Make sure all nodes appear in the in-degree dictionary
-            if index not in in_degree:
-                in_degree[index] = 0
-
-        # Step 3: Find all nodes with no incoming edges (in-degree 0)
-        zero_in_degree_nodes = deque(
-            [node for node in in_degree if in_degree[node] == 0]
-        )
-
-        # Step 4: Perform the topological sort
-        topo_sorted = []
-        while zero_in_degree_nodes:
-            current_node = zero_in_degree_nodes.popleft()
-            topo_sorted.append(current_node)
-
-            # Decrease the in-degree of each descendant
-            for neighbor in graph[current_node]:
-                in_degree[neighbor] -= 1
-                if in_degree[neighbor] == 0:
-                    zero_in_degree_nodes.append(neighbor)
-
-        # Step 5: Check if topological sort is possible (no cycle)
-        if len(topo_sorted) == len(self.nodes):
-            return topo_sorted
-        else:
-            return ("Cycle detected, topological sort not possible",list(range(len(self.nodes))))[1]
-
-def play(agent:network) -> int:
-	"""play against the agent where it's opponent is a randomized move and return the score"""
-	"""the scoring will be based +1 if it attacks on block that has no other attack"""
-	"""else -1, +2 if tie,-5 if loose,+10 if wins,- amount of total attack made"""
-	
-
-#def sort_by_score(agents:list[list['agent:network',"score:int"]]) -> list[list[int,int]]:
-#	"""sort agents by it's score"""
-#	print(agents)
-#	
-#	l = len(agents)
-#	s=  agents
-#	while True:
-#		agents1 = deepcopy(agents)
-#		agents = []
-#		for agentID,(_,score) in enumerate(agents1):
-#			jmbl = False
-#			#print(agentID, (agentID+2)%l,l)
-#			a = agents1[agentID]
-#			b = agents1[(agentID+2)%l]
-#			
-#			if a[1] > b[1]:
-#				temp = a
-#			else:
-#				jmbl = True
-#				temp = b
-#			agents.append(temp)
-#			#print(agents)
-#		if not jmbl:
-#			break
-#			
-#	return agents
-
-def pop_range(lst, start, end):
-#    if start < 0 or end >= len(lst) or start > end or isinstance(start,int):
-#        raise ValueError("Invalid start or end index")
-    result = lst[start:end+1]
-    del lst[start:end+1]
-    return result
-
-
-
-
-#random.seed(2)
-#print(agent_sort_by_score([[0, random.randint(-10,10)] for _ in range(5)]))
-#random.seed(2)
-#a = [[0, random.randint(-10,10)] for _ in range(5)]
-#a = sorted(a,key= lambda x: x[1], reverse=True)
-#print(a)
+random.seed(0)
 
 def main():
-	from timeit import timeit
-	
-	num = 1_000
-	
-	#print(timeit("agent_sort_by_score(a)", number=num, globals=globals()))
-	#print(timeit("sorted(a,key= lambda x: x[1], reverse=True)", number=num, globals=globals()))
-	#print(timeit("a.sort(key= lambda x: x[1], reverse=True);s = a", number=num, globals=globals()))
-	
-	
-	flat2D = lambda x: [a for q in x for a in q]
-	###################
-	
-	
-	x = "x"
-	o = "o"
-	n = ""
-	
-	#input
-	i = [[x,n,x],
-	     [o,o,x],
-	     [n,n,n]]
-	     
-	s = [[-1.0 if x == a else 1.0 if o == a else 0.001 for a in q] for q in i]
-	
-	s = flat2D(s)
-	
-	seed = 1
-	
-	inputs = 9
-	outputs = 2
-	
-	HLayer = [8,9,5]
-	
-	dag = network(inputs,outputs)
-	
-	print("input:",i,"translated:",s,sep="\n")
-	
-	total = sum(HLayer)+inputs+outputs
-	#do nothing yet
-	for nodeLen in HLayer:
-		for i in range(nodeLen):
-			pass
-	
-	#create the interconnection
-	#make a random weight for each connection 
-	random.seed(seed)
-	for i in range(inputs):
-		for j in range(outputs):
-			dag.add_connection(i,j,random.randint(-5,5))
-	
-	
-	#random bias for all node
-	random.seed(seed)
-	for i in dag.topoSort():
-		dag.change_bias(i,random.randint(-5,5))
-	
-	
-	print("output\n  ",dag.ev(s))
-	#print("nodes",dag.nodes)
-	print(f"total nodes {len(dag.nodes)}")
-	
-	random.seed(seed)
-	print(random.randint(0,5))
+    dag = Network(2,2)
+    dag.mutate(1)
+    a = [2,2]
+    dag.add_connection(1,3,1)
+    print(dag.inp,dag.outp,dag.hidden_nodes,dag.connections)
+    print(dag.predict(a))
 
 
-if __name__ = "__main__":
-	main()
+    
 
-import random
+class Network:
+    def __init__(self, inp, outp):
+        self.inp = inp
+        self.outp = [[0,"tanh"] for _ in range(outp)]
+        self.hidden_nodes = []
+        self.connections = {}
+    def predict(self, env:list):
+        '''create a prediction based on the structure of the network\nenv is a list of measurement from the environment'''
+        outp = [0.0]*len(self.outp)
+        #storing the final values
+        values = {}
+        #values = {descendantID: {parentID: val} for parentID,val in enumerate(env) for _,descendantID in self.connections[parentID].items() if parentID in self,connections}
+        for parentID,val in enumerate(env):
+            if parentID in self.connections:
+                for descendantID,w in self.connections[parentID].items():
+                    if descendantID in values:
+                        values[descendantID][parentID] = val
+                    else:
+                        values[descendantID] = {parentID: val}
+        
+        #how many out node
+        outn = len(self.outp)
+        
+        #step 1 get the order of operation
+        srtd = self.topoSort()
+        print(srtd)
+        for i in srtd[self.inp:]:
+            
+            #check is its a output node by knowing if node index(i) is less than outn
+            #index format is input_nodes,output_nodes,hidden_nodes
+            a_outp_node = not i < self.inp
+            
+            #holder of biases
+            
+            node = self.outp[i-self.inp-1] if a_outp_node else self.hidden_nodes[i-(outn+self.inp)-1]
+            # if i in self.connections:
+            #     print(i,node,values[i],self.connections[i],"lll")
+            
+            
+            #calculate the final value
+            val = sum(
+                [
+                    (
+                        0 if descendantID not in values else
+                        0 if i not in values[descendantID] else values[descendantID][i]
+                    ) * w
+                    for descendantID,w in self.connections[i].items()
+                ]
+            ) + node[0] if i in self.connections else 0
+            
+            #activator chunk
+            activator = node[1]
+            if activator == "relu":
+                val = 0 if val < 0 else val
+            elif activator == "sigmoid":
+                val = 1/(1+exp(-val))
+            elif activator == "tanh":
+                val = tanh(val)
+            elif activator == "softmax":
+                pass
+            
+            #stuff that will tell what value a node will get
+            print(val)
+            if not a_outp_node:
+                for dest,_ in self.connections[i].items():
+                    if i in values:
+                        values[dest][i] = val
+                    else:
+                        values[dest] = {i: val}
+            
+            else:
+                outp[i-self.inp] = val
+        
+        return outp
+        
+    def mutate(self, new_connection_rate: int, new_node_rate: int = 0, new_bias_rate: int = 0):
+        """do a random mutation on the network None means give no probability that a action rate will be performed"""
+        
+        #create a list of how much a action will be done
+        lst = ["connection"] * new_connection_rate
+        lst += ["node"] * new_node_rate
+        lst += ["bias"] * new_bias_rate
+        
+        #randomly select on what action will be done
+        action = random.choice(lst)
+        
+        hidden = len(self.hidden_nodes)
+        outp = len(self.outp)
+        #create a list on IDs of all nodes
+        total_nodes = self.inp+outp+hidden
+        nodes_ID = list(range(total_nodes))
+        
+        #get a random poiting node
+        node = random.choice(nodes_ID)
+        #this is a node that will be use as head for a connection
+        srcs_node = random.choice(nodes_ID[:self.inp] + nodes_ID[self.inp+outp:])
+        #get a random dest from outp to hidden where dest is always the descendant of the srcs_node
+        dest =  nodes_ID[(srcs_node if srcs_node < self.inp-1 else self.inp)-1:]
+        dest_node = random.choice(dest)
+        
+        #create a random bias and wieghts
+        r1,r2,r3 = [random.randint(-7,7) for _ in (1,2,3)]
+        
+        print(action,srcs_node,dest_node,r1)
+        if action == "node" and self.connections:
+            self.add_node(srcs_node, dest_node, w1, w2, w3)
+            #this should be same as connection option
+        elif action == "bias":
+            self.bias(node, w1)
+            #get a random node from hidden to output
+        else:
+            self.add_connection(srcs_node, dest_node, r1)
+            #random node from input to hidden, from the index of the chosen hidden to output where index cant be pointing at inp node
+    def add_node(self, node: int, dest: int, bias = 0.0, w1 = 1.0, w2 = 1.0, activator: str = "relu"):
+        """adds new node by spiting connection assuming/n     that you call this and there is connection exist else err"""
+        
+        #step 1 add the hidden_node on the node list
+        self.hidden_nodes.append([bias,activator])
+        new_node_id = len(self.hidden_nodes)-1
+        
+        self.add_connection(node, new_node_id, w1)
+        self.add_connection(new_node_id, dest, w2)
+    def add_connection(self, node:int, dest:int, w = 1.0):
+        if node in self.connections:
+            self.connections[node][dest] = w
+        else:
+            self.connections[node] = {dest: w}
+    def change_bias(self, node:int, bias = 0.0):
+        #if the node is a hidden node or a output
+        layer = self.hidden_nodes if node > len(self.outp) else self.outp
+        layer[node] = bias
+    
+    def topoSort(self):
+        edge = self.connections
+        list_of_all_node  = range(self.inp+len(self.outp+self.hidden_nodes))
+        # Add all nodes from list_of_all_node to the edge dictionary if they don't already exist
+        for node in list_of_all_node:
+            if node not in edge:
+                edge[node] = {}
+    
+        # Initialize a dictionary to count in-degrees of all nodes
+        in_degree = {node: 0 for node in list_of_all_node}
+    
+        # Calculate in-degrees
+        for node in edge:
+            for neighbor in edge[node]:
+                if neighbor in in_degree:
+                    in_degree[neighbor] += 1
+    
+        # Initialize a queue and add all nodes with in-degree 0
+        queue = [node for node in in_degree if in_degree[node] == 0]
+    
+        topo_sorted_list = []
+    
+        while queue:
+            current_node = queue.pop(0)
+            topo_sorted_list.append(current_node)
+    
+            for neighbor in edge[current_node]:
+                in_degree[neighbor] -= 1
+                if in_degree[neighbor] == 0:
+                    queue.append(neighbor)
+    
+        if len(topo_sorted_list) == len(list_of_all_node):
+            return topo_sorted_list
+        else:
+            raise Exception("Graph has a cycle")
+    
+    # def topoSort(self):
+    #     # Step 1: Initialize the graph and in-degree dictionary
+    #     graph = defaultdict(list)
+    #     in_degree = defaultdict(int)
+        
+    #     # Step 2: Populate the graph and in-degree based on input_data
+    #     for index, descendants in self.connections.items():
+    #         if descendants:
+    #             for descendant in descendants:
+    #                 if not descendant:
+    #                     continue
+                    
+    #                 graph[index].append(descendant)
+    #                 in_degree[descendant] += 1
+            
+    #         # Make sure all nodes appear in the in-degree dictionary
+    #         if index not in in_degree:
+    #             in_degree[index] = 0
+        
+    #     # Step 3: Find all nodes with no incoming edges (in-degree 0)
+    #     zero_in_degree_nodes = deque(
+    #         [node for node in in_degree if in_degree[node] == 0]
+    #     )
+        
+    #     # Step 4: Perform the topological sort
+    #     topo_sorted = []
+    #     while zero_in_degree_nodes:
+    #         current_node = zero_in_degree_nodes.popleft()
+    #         topo_sorted.append(current_node)
+            
+    #         # Decrease the in-degree of each descendant
+    #         for neighbor in graph[current_node]:
+    #             in_degree[neighbor] -= 1
+    #             if in_degree[neighbor] == 0:
+    #                 zero_in_degree_nodes.append(neighbor)
+        
+    #     # Step 5: Check if topological sort is possible (no cycle)
+    #     # print(topo_sorted)
+    #     # if len(topo_sorted) == len(self.connections):
+    #     #     return topo_sorted
+    #     # else:
+    #     #     raise ValueError("topological sorting is impossible")
+        # return topo_sorted
 
-#========tic tac toe training code============
-is_board_full = lambda board: (all(cell != '' for cell in board.copy()))
-available_move = lambda board: [i for i,c in enumerate(board) if c == '']
 
-win_combo = [
-	[0,1,2],[3,4,5],[6,7,8],
-	[0,3,6],[1,4,7],[2,5,8],
-	[0,4,8],[2,4,6]
-]
-
-def is_winner(player,board):
-	for c in win_combo:
-		if all(board[i]==player for i in c.copy()):
-			return True
-	return False
-	
-def play(board):
-	
-	a = input().split(",")
-	a = [int(i) for i in a]
-#	a=((y+1)*3)+(x+1)
-#	print(a)
-	return a
-
-def dplay(agent):
-	y,x = [i+1 for i in agent]
-	return (y*3)+x
-
-def fit(network, is_agent_attack_x=True, fitness_seed:int=0) -> int:
-	random.seed(fitness_seed)
-	board = [""]*9
-	cboard = [0]*9
-	
-	score = 0
-	while not is_board_full(board):
-		
-		score -= 1
-		x_move = dplay(network.run(cboard)) if is_agent_attack_x else random.choice(available_move(board))
-		
-		a = (1,-1) if is_agent_attack_x else (-1,1)
-		
-		if board[x_move] == "":
-			board[x_move] = "x"
-			cboard[x_move] = a[0]
-			if is_winner("x",board):
-				print("x won")
-				score -= 20
-				break
-			if is_board_full(board):
-				print("tie")
-				break
-			o_move = random.choice(available_move(board)) if is_agent_attack_x else dplay(network.run(cboard))
-			print(o_move)
-			board[o_move] = "o"
-			cboard[o_move] = a[1]
-			if is_winner("o",board):
-				print("o won")
-				score += 100
-				break
-		else:
-			print("oof wrong attack ",x_move,board[x_move])
-			return score + 999
-		for i in range(0,9,3):
-			print(board[i:i+3])
-	
-	return score
-#========tic tac toe training code============
+if __name__ == "__main__":
+    main()
